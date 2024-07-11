e638b5609ea24001948747b30dd0e06b6ebe1ee9
JTshonda <TSHONDAJUBILEE@GMAIL.COM>
2024-07-11 15:39:19 +0200

    added some java script
    added an event listener, change the name of the city to the city user input.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function searchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#input-city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", searchCity);

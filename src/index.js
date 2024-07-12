6c0a70fd2c3f2a755087b9b82fd9136e3660c2fe
JTshonda <TSHONDAJUBILEE@GMAIL.COM>
2024-07-12 13:29:26 +0200

    added a weather API
    connected to weather API change the temperature to the city searched for

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+function newCityWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+}
+
 function searchCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#input-city");
-  cityElement.innerHTML = searchInput.value;
+  let city = searchInput.value;
+  let apiKey = "1d99d901f34da597o23a408fbateb9dc";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(newCityWeather);
 }
 
 let searchFormElement = document.querySelector("#search-form");

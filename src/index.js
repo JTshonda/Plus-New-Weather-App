07e1dc54b6d874b460cb4e465c0be763810c79f0
JTshonda <TSHONDAJUBILEE@GMAIL.COM>
2024-07-12 13:47:53 +0200

    change weather details

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ function newCityWeather(response) {
   temperatureElement.innerHTML = Math.round(temperature);
   let cityElement = document.querySelector("#current-city");
   cityElement.innerHTML = response.data.city;
+  let description = document.querySelector("#temp-description");
+  description.innerHTML = response.data.condition.description;
+  let humidity = document.querySelector("#humidity-level");
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  let windElement = document.querySelector("#wind-speed");
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  let timeElement = document.querySelector("#date-time");
+  let date = new Date(response.data.time * 1000);
+  timeElement.innerHTML = formatDate(date);
+  let iconElement = document.querySelector("#icon");
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
 function searchCity(event) {

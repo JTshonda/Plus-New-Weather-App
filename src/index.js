376277f6d826aeedf521d0b3befc9c2e7fd39b2d
JTshonda <TSHONDAJUBILEE@GMAIL.COM>
2024-07-12 14:10:20 +0200

    gave id names, formatted date
    added a id to date, humidity, windspeed, weather icon and temperature unit. formatted the date is java script

--- src/index.js
+++ src/index.js
@@ -10,13 +10,32 @@ function newCityWeather(response) {
   humidity.innerHTML = `${response.data.temperature.humidity}%`;
   let windElement = document.querySelector("#wind-speed");
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  let iconElement = document.querySelector("#temp-icon");
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon" />`;
   let timeElement = document.querySelector("#date-time");
   let date = new Date(response.data.time * 1000);
   timeElement.innerHTML = formatDate(date);
-  let iconElement = document.querySelector("#icon");
-  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[date.getDay()];
 
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
 function searchCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
